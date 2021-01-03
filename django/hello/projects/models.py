78d0064f5e1a54f4c83dc8b2f460350f5a9237dc
Sofia <kartikasofia35@gmail.com>
2021-01-03 20:15:53 +0700

    oke
    upload

---
+++ django/hello/projects/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.title
